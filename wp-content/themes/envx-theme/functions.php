ce5ba0a8901ae7f36ec0fb94c608c8f012d4bc50
untalanlem <untalanlem@gmail.com>
2021-08-02 00:07:24 +0800

    Change the function name of the enqueuing function in the functions.php

--- wp-content/themes/envx-theme/functions.php
+++ wp-content/themes/envx-theme/functions.php
@@ -1,9 +1,9 @@
 <?php
 
 // Enqueue CSS and JS
-function underscores_scripts() {
+function envx_assets() {
   $version = '1.0.0';
 	wp_enqueue_style( 'envx-style', get_stylesheet_uri() . '/css/styles.css', array(), $version );
   wp_enqueue_script( 'envx-js', get_stylesheet_uri() . 'js/script.js', array(), $version, true );
 }
-add_action( 'wp_enqueue_scripts', 'underscores_scripts' );
+add_action( 'wp_enqueue_scripts', 'envx_assets' );
